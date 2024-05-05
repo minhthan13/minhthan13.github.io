eb5249ee243ec040034e673bf7f896a6c912511d
Krantz <nguyenminhthan1308@gmail.com>
2024-05-05 17:54:32 +0700

    deploy angular

---
+++ docs/a1/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-5JCYRP2Q.mjs";import"./chunk-S6YZQDOA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ docs/a2/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YLCUJGUU.mjs";import"./chunk-S6YZQDOA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
