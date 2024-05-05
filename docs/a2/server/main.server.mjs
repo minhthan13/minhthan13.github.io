fe2e953e805ded579900fc092fbb4ba545a21c56
Krantz <nguyenminhthan1308@gmail.com>
2024-05-05 18:19:21 +0700

    clear repo

--- docs/a2/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-YLCUJGUU.mjs";import"./chunk-S6YZQDOA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- docs/a3/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-6W3TGDCE.mjs";import"./chunk-S6YZQDOA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
