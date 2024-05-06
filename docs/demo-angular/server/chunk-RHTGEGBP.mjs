0576f7badd8a92b6e8214c50740972397fcf36b2
Krantz <nguyenminhthan1308@gmail.com>
2024-05-06 20:10:55 +0700

    deploy with config tsconfig.json

---
+++ docs/demo-angular/server/chunk-RHTGEGBP.mjs
@@ -0,0 +1,16 @@
+import './polyfills.server.mjs';
+import{A as y,G as E,a as g,b as c,c as o,d as h,e as f,f as i,g as n,h as p,i as l,j as u,k as e,l as d,m,n as x,u as v,w as C,x as S}from"./chunk-NQEQR5HI.mjs";var I=[];var _={providers:[E(I),S()]};var T={providers:[y()]},D=v(_,T);function q(r,a){r&1&&(i(0,"span",2),e(1,"Show"),n())}function M(r,a){r&1&&(i(0,"span",5),e(1,"Hide"),n())}function P(r,a){r&1&&(i(0,"span",4),e(1,"A"),n())}function z(r,a){r&1&&(i(0,"span",6),e(1,"B"),n())}function A(r,a){r&1&&(i(0,"span",7),e(1,"C"),n())}function O(r,a){r&1&&(i(0,"span",5),e(1,"D"),n())}var w=(()=>{let a=class a{ngOnInit(){this.id=1,this.username="minhthan13",this.status=!0,this.phone="+84902518533",this.price=3.2,this.quantity=40,this.photo="Docker.jpeg",this.width=100,this.height=100,this.product={id:1,name:"product 1",photo:"Docker.jpeg",price:3.2,quantity:10},this.score=6.4}};a.\u0275fac=function(s){return new(s||a)},a.\u0275cmp=g({type:a,selectors:[["app-demo1"]],standalone:!0,features:[x],decls:56,vars:21,consts:[[1,"format"],["alt","",3,"src","width","height"],[2,"color","green"],["border","1"],[2,"color","blue"],[2,"color","red"],[2,"color","yellow"],[2,"color","orange"]],template:function(s,t){s&1&&(i(0,"h2",0),e(1,"demo1 Component!"),n(),p(2,"hr"),e(3),p(4,"hr"),e(5),p(6,"hr"),e(7),p(8,"hr"),e(9),p(10,"hr"),e(11),p(12,"hr"),e(13),p(14,"hr"),e(15),p(16,"hr")(17,"img",1)(18,"hr"),h(19,q,2,0,"span",2)(20,M,2,0),p(21,"hr"),i(22,"span"),e(23,"Product info"),n(),i(24,"table",3)(25,"tr")(26,"td"),e(27,"id"),n(),i(28,"td"),e(29),n()(),i(30,"tr")(31,"td"),e(32,"name"),n(),i(33,"td"),e(34),n()(),i(35,"tr")(36,"td"),e(37,"photo"),n(),i(38,"td"),p(39,"img",1),n()(),i(40,"tr")(41,"td"),e(42,"price"),n(),i(43,"td"),e(44),n()(),i(45,"tr")(46,"td"),e(47,"quantity"),n(),i(48,"td"),e(49),n()()(),p(50,"hr")(51,"br"),h(52,P,2,0,"span",4)(53,z,2,0)(54,A,2,0)(55,O,2,0)),s&2&&(o(3),m(`
+id: `,t.id,`
+`),o(2),m(`
+Username : `,t.username,`
+`),o(2),m(`
+status : `,t.status?"show":"hide",`
+`),o(2),m(`
+Phone : `,t.phone,`
+`),o(2),m(`
+price : `,t.price,`
+`),o(2),m(`
+Quantity : `,t.quantity,`
+`),o(2),m(`
+total : `,t.price*t.quantity,`
+`),o(2),u("src","assets/images/",t.photo,"",c),l("width",t.width),l("height",t.height),o(2),f(19,t.status?19:20),o(10),d(t.product.id),o(5),d(t.product.name),o(5),u("src","assets/images/",t.product.photo,"",c),l("width",t.width),l("height",t.height),o(5),d(t.product.price),o(5),d(t.product.quantity),o(3),f(52,t.score>=8?52:t.score>=7?53:t.score>=6?54:55))},styles:["img[_ngcontent-%COMP%]{border:2px solid red}",".format[_ngcontent-%COMP%]{color:#2e59b6;font-size:30px}"]});let r=a;return r})();var U=()=>C(w,D),X=U;export{X as a};
---
+++ docs/demo-angular/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-RHTGEGBP.mjs";import"./chunk-NQEQR5HI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
